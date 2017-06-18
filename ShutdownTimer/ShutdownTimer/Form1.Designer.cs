cb3f7906aef926eaf736ca7d03f22b4e8cfbf396
thuongthanhto <thuongthanhto@gmail.com>
2017-06-18 10:52:49 +0700

    xong chương trình hẹn giờ tắt máy

---
+++ ShutdownTimer/ShutdownTimer/Form1.Designer.cs
@@ -0,0 +1,155 @@
+﻿namespace ShutdownTimer
+{
+    partial class frmMain
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.cboTime = new System.Windows.Forms.ComboBox();
+            this.btnTimer = new System.Windows.Forms.Button();
+            this.btnDisTimer = new System.Windows.Forms.Button();
+            this.pnlTitle = new System.Windows.Forms.Panel();
+            this.btnExit = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.pnlTitle.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // cboTime
+            // 
+            this.cboTime.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.cboTime.DropDownWidth = 100;
+            this.cboTime.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.cboTime.Location = new System.Drawing.Point(60, 60);
+            this.cboTime.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.cboTime.Name = "cboTime";
+            this.cboTime.Size = new System.Drawing.Size(160, 25);
+            this.cboTime.TabIndex = 0;
+            // 
+            // btnTimer
+            // 
+            this.btnTimer.BackColor = System.Drawing.Color.LimeGreen;
+            this.btnTimer.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnTimer.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.btnTimer.Location = new System.Drawing.Point(250, 58);
+            this.btnTimer.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.btnTimer.Name = "btnTimer";
+            this.btnTimer.Size = new System.Drawing.Size(100, 36);
+            this.btnTimer.TabIndex = 1;
+            this.btnTimer.Text = "Hẹn giờ";
+            this.btnTimer.UseVisualStyleBackColor = false;
+            this.btnTimer.Click += new System.EventHandler(this.btnTimer_Click);
+            // 
+            // btnDisTimer
+            // 
+            this.btnDisTimer.BackColor = System.Drawing.Color.Red;
+            this.btnDisTimer.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnDisTimer.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.btnDisTimer.Location = new System.Drawing.Point(250, 111);
+            this.btnDisTimer.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.btnDisTimer.Name = "btnDisTimer";
+            this.btnDisTimer.Size = new System.Drawing.Size(100, 35);
+            this.btnDisTimer.TabIndex = 2;
+            this.btnDisTimer.Text = "Hủy";
+            this.btnDisTimer.UseVisualStyleBackColor = false;
+            this.btnDisTimer.Click += new System.EventHandler(this.btnDisTimer_Click);
+            // 
+            // pnlTitle
+            // 
+            this.pnlTitle.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(213)))), ((int)(((byte)(219)))), ((int)(((byte)(233)))));
+            this.pnlTitle.Controls.Add(this.label1);
+            this.pnlTitle.Controls.Add(this.btnExit);
+            this.pnlTitle.Dock = System.Windows.Forms.DockStyle.Top;
+            this.pnlTitle.Location = new System.Drawing.Point(0, 0);
+            this.pnlTitle.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.pnlTitle.Name = "pnlTitle";
+            this.pnlTitle.Size = new System.Drawing.Size(406, 35);
+            this.pnlTitle.TabIndex = 3;
+            this.pnlTitle.MouseDown += new System.Windows.Forms.MouseEventHandler(this.pnlTitle_MouseDown);
+            this.pnlTitle.MouseMove += new System.Windows.Forms.MouseEventHandler(this.pnlTitle_MouseMove);
+            this.pnlTitle.MouseUp += new System.Windows.Forms.MouseEventHandler(this.pnlTitle_MouseUp);
+            // 
+            // btnExit
+            // 
+            this.btnExit.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(213)))), ((int)(((byte)(219)))), ((int)(((byte)(233)))));
+            this.btnExit.Dock = System.Windows.Forms.DockStyle.Right;
+            this.btnExit.FlatAppearance.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(255)))), ((int)(((byte)(192)))));
+            this.btnExit.FlatAppearance.BorderSize = 0;
+            this.btnExit.FlatAppearance.MouseDownBackColor = System.Drawing.Color.Red;
+            this.btnExit.FlatAppearance.MouseOverBackColor = System.Drawing.Color.Red;
+            this.btnExit.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnExit.Image = global::ShutdownTimer.Properties.Resources.ic_close_black_24dp;
+            this.btnExit.Location = new System.Drawing.Point(363, 0);
+            this.btnExit.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.btnExit.Name = "btnExit";
+            this.btnExit.Size = new System.Drawing.Size(43, 35);
+            this.btnExit.TabIndex = 0;
+            this.btnExit.UseVisualStyleBackColor = false;
+            this.btnExit.Click += new System.EventHandler(this.btnExit_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Times New Roman", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(13, 5);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(140, 22);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Hẹn giờ tắt máy";
+            // 
+            // frmMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 17F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(406, 178);
+            this.Controls.Add(this.pnlTitle);
+            this.Controls.Add(this.btnDisTimer);
+            this.Controls.Add(this.btnTimer);
+            this.Controls.Add(this.cboTime);
+            this.Font = new System.Drawing.Font("Times New Roman", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.Name = "frmMain";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Shutdown Timer";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.frmMain_FormClosing);
+            this.pnlTitle.ResumeLayout(false);
+            this.pnlTitle.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ComboBox cboTime;
+        private System.Windows.Forms.Button btnTimer;
+        private System.Windows.Forms.Button btnDisTimer;
+        private System.Windows.Forms.Panel pnlTitle;
+        private System.Windows.Forms.Button btnExit;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ ShutdownTimer/ShutdownTimer/Form1.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ShutdownTimer
+{
+    public partial class frmMain : Form
+    {
+        Process cmdProcess; // Tiến trình của cmd
+
+        // Dùng để di chuyển form vì mặc định form không di chuyển được 
+        int _togMove; // Cho biết pnlTitle có được click chuột vào hay không (1 là có, 0 là không có)
+        int _mValX; // Khoảng cách chuột di chuyển theo phương x
+        int _mValY; // Khoảng cách chuột di chuyển theo phương y
+
+        public frmMain()
+        {
+            InitializeComponent();
+
+            // Khởi tạo combobox.
+            Dictionary<string, string> dict = new Dictionary<string, string>();
+            for (int i = 30; i <= 180; i += 30)
+            {
+                string temp = i.ToString();
+                dict.Add(temp, temp);
+            }
+            cboTime.DataSource = new BindingSource(dict, null);
+            cboTime.DisplayMember = "Value";
+            cboTime.ValueMember = "Key";
+            
+            // Khởi tạo process.
+            cmdProcess = new Process();
+        }
+
+        private void btnTimer_Click(object sender, EventArgs e)
+        {
+            string value = ((KeyValuePair<string, string>)cboTime.SelectedItem).Value;
+            ProcessStartInfo proc = new ProcessStartInfo("cmd", "/c shutdown -s -t " + (int.Parse(value)*60))
+            {
+                RedirectStandardOutput = true,
+                UseShellExecute = false,
+                CreateNoWindow = true
+            };
+
+            cmdProcess.StartInfo = proc;
+            cmdProcess.Start();
+            cmdProcess.WaitForExit();
+        }
+
+        private void btnDisTimer_Click(object sender, EventArgs e)
+        {
+            ProcessStartInfo proc = new ProcessStartInfo("cmd", "/c shutdown -a")
+            {
+                RedirectStandardOutput = true,
+                UseShellExecute = false,
+                CreateNoWindow = true
+            };
+
+            cmdProcess.StartInfo = proc;
+            cmdProcess.Start();
+            cmdProcess.WaitForExit();
+        }
+
+        private void frmMain_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            cmdProcess.Close();
+        }
+
+        private void pnlTitle_MouseDown(object sender, MouseEventArgs e)
+        {
+            _togMove = 1;
+            _mValX = e.X;
+            _mValY = e.Y;
+        }
+
+        private void pnlTitle_MouseUp(object sender, MouseEventArgs e)
+        {
+            _togMove = 0;
+        }
+
+        private void pnlTitle_MouseMove(object sender, MouseEventArgs e)
+        {
+            if (_togMove == 1)
+            {
+                this.SetDesktopLocation(MousePosition.X - _mValX, MousePosition.Y - _mValY);
+            }
+        }
+
+        private void btnExit_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+    }
+}
